11c1ff31ddd781272836b913ee6c20c463a4c2f7
Daniel <daniel.torresg@udea.edu.co>
2020-03-05 06:53:23 -0500

    Captura punto 3

--- Apuntadores/Punto3.c
+++ Apuntadores/Punto3.c
@@ -6,45 +6,19 @@ char *obtenerSubcadena(char *array, int index);
 
 
 int main() {
-    printf("Ingrese la cadena:");
+    printf("Ingrese la cadena: ");
     char hola [20] = "Hola";
     scanf("%s", hola);
     int num;
-    printf("Ingrese el número para cortar:");
+    printf("Ingrese el número para cortar: ");
     scanf("%d", &num);
     char *subCadena;
     subCadena = obtenerSubcadena(hola, num);
-    printf("\n Cadena :%s \n", subCadena);
+    printf("Cadena :%s \n", subCadena);
 
 }
 
-char *obtenerSubcadena(char *array, int index){
-    char * subCadena;
-    char *dir = array;
-    int tamano = strlen(array) - index;
-    subCadena = malloc(sizeof(char)*tamano);
-    char aux[200];
-    if(index > strlen(array)){        
-        return NULL;
-    }
-
-
-    for (int i = 0; i < index; i++)
-    {
-        *dir++;
-    }
-
-    int i = 0;
-
-    while (*dir != '\0') {
-        aux[i] = dir[0];
-        dir++;
-        i++;
-    }
-    
-    printf("El oroginal es: %s", array);
-    subCadena = aux;
-    return subCadena; 
-
-
+char *obtenerSubcadena(char *array, int index) {
+    array = array + index;
+    return array;
 }
